23fbc861eb6304a118349b2c728f28f1e933159c
aaronstuder <aaronstuder@gmail.com>
2020-07-30 20:49:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0ac80df6eff0e70b5"
-  instance_type = "t3a.micro"
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0ac80df6eff0e70b5"
+#  instance_type = "t3a.micro"
+#}
