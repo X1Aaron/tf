ba2dc3f5207c4ead6201292c91b1f0ab3158b877
aaronstuder <aaronstuder@gmail.com>
2020-07-31 07:52:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 resource "aws_spot_instance_request" "pihole" {
   ami           = "ami-0ac80df6eff0e70b5"
+  spot_price    = "0.01"
   instance_type = "t3a.nano"
   instance_interruption_behaviour = "stop"
   key_name   = "aaronstuder"
